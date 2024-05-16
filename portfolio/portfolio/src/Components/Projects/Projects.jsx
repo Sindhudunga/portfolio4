270acc4f63cad4603ecef03596b311c5d35f385d
josnavi dunga <154753673+Sindhudunga@users.noreply.github.com>
2024-05-16 19:02:05 +0530

    Add files via upload

---
+++ portfolio/portfolio/src/Components/Projects/Projects.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Projects.css";
+
+const Projects = () => {
+  return (
+    <div className="container-fluid my-5 py-5 text-center" id="projects">
+      <h3 className="py-5 ">
+        My <span>Projectss</span>...
+      </h3>
+      <div className="container my-5">
+        <div class="row">
+          <div class="col-12 col-md-6 px-4 py-4">
+            <img
+              src="../src/assets/project 1.jpg"
+              alt="Project 1"
+              className="img-fluid shadow rounded projImg"
+            />
+          </div>
+          <div class="col-12 col-md-6 px-4 py-4">
+            <img
+              src="../src/assets/project 2.png"
+              alt="Project 1"
+              className="img-fluid shadow rounded projImg"
+            />
+          </div>
+          <div class="col-12 col-md-6 px-4 py-4">
+            <img
+              src="../src/assets/project 4.png"
+              alt="Project 1"
+              className="img-fluid shadow rounded projImg"
+            />
+          </div>
+          <div class="col-12 col-md-6 px-4 py-4">
+            <img
+              src="../src/assets/project 3.png"
+              alt="Project 1"
+              className="img-fluid shadow rounded projImg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
